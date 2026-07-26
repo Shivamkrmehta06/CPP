50df5ed63cab27f8fbd56041e7eb67850c59e76a
Shivam Kumar Mehta <shivamkrmehta@outlook.com>
2026-07-26 16:57:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (29.38%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> s1;
+        stack<char> s2;
+        for(auto x:s){
+            if(x=='#'){
+                if(!s1.empty()) s1.pop();
+            }else{
+                s1.push(x);
+            }
+        }
+        for(auto x:t){
+            if(x=='#'){
+                if(!s2.empty()) s2.pop();
+            }else{
+                s2.push(x);
+            }
+        }
+        return s1==s2;
+    }
+};
