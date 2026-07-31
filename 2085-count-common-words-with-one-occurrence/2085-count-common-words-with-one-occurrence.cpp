73d5637f22e1a6a892829392cf5d305374aa8e64
Shivam Kumar Mehta <shivamkrmehta@outlook.com>
2026-07-31 21:15:00 +0530

    Time: 3 ms (96.05%), Space: 20.6 MB (91.43%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string,int> ump1;
+        unordered_map<string,int> ump2;
+        for(int i=0;i<words1.size();i++){
+            ump1[words1[i]]++;
+        }
+        for(int i=0;i<words2.size();i++){
+            if(ump1.count(words2[i])) ump2[words2[i]]++;
+        }
+        int count = 0;
+        for(auto x:ump2){
+            if(ump1.count(x.first)&&x.second==1&&ump1[x.first]==1) count++; 
+        }
+        cout<<count<<endl;
+        return count; 
+    }
+};
