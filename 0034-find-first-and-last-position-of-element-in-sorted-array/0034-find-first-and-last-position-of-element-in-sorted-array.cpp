94764a2c2c63888372431c51da9a007b199a2da2
Shivam Kumar Mehta <shivamkrmehta@outlook.com>
2026-08-14 13:34:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,44 @@
 class Solution {
 private:
-    int binarySearchL(vector<int> nums,int start,int end,int target){
-        int mid = start+(end-start)/2;
-        int ans = -1;
-        while(start<=end){
-            if(nums[mid]==target){
-                ans = mid;
-                end = mid-1;
-            }else if(nums[mid]>target){
-                end = mid-1;
+    int firstOccurenece(vector<int> &arr,int target){
+        int low=0;
+        int high=arr.size()-1;
+        int ans=-1;
+        int mid = low+(high-low)/2;
+        while(low<=high){
+            if(arr[mid]==target){
+                ans=mid;
+                high = mid-1;
+            }else if(arr[mid]<target){
+                low=mid+1;
             }else{
-                start = mid+1;
+                high=mid-1;
             }
-            mid = start+(end-start)/2;
+            mid=low+(high-low)/2;
         }
         return ans;
     }
-    int binarySearchR(vector<int> nums,int start,int end,int target){
-        int mid = start+(end-start)/2;
-        int ans = -1;
-        while(start<=end){
-            if(nums[mid]==target){
-                ans = mid;
-                start = mid+1;
-            }else if(nums[mid]>target){
-                end = mid-1;
+
+    int lastOccurenece(vector<int> &arr,int target){
+        int low=0;
+        int high=arr.size()-1;
+        int ans=-1;
+        int mid = low+(high-low)/2;
+        while(low<=high){
+            if(arr[mid]==target){
+                ans=mid;
+                low = mid+1;
+            }else if(arr[mid]<target){
+                low=mid+1;
             }else{
-                start = mid+1;
+                high=mid-1;
             }
-            mid = start+(end-start)/2;
+            mid=low+(high-low)/2;
         }
         return ans;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int xi = binarySearchL(nums,0,nums.size()-1,target);
-        int xe = binarySearchR(nums,0,nums.size()-1,target);
-        ans.push_back(xi);
-        ans.push_back(xe);
-        return ans;
+        return {firstOccurenece(nums,target),lastOccurenece(nums,target)};
     }
 };
