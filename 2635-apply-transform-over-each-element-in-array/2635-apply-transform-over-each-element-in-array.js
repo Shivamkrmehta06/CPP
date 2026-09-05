cfdcf8704efd3e9b877445350fc6ef556e07984b
Shivam Kumar Mehta <shivamkrmehta@outlook.com>
2026-09-06 00:51:15 +0530

    Time: 39 ms (79.82%), Space: 53.5 MB (55.18%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const result=[]
+    arr.forEach((item,index)=>result.push(fn(item,index)))
+    return result;
+};
